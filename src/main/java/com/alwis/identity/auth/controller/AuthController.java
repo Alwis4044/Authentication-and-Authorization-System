b929360dc2b9240e57189249b3eb211cba9595b4
Alwis Shaji <alwis562@gmail.com>
2026-03-30 18:28:38 +0530

    Implement authentication APIs (register & login)

---
+++ src/main/java/com/alwis/identity/auth/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.alwis.identity.auth.controller;
+
+import com.alwis.identity.auth.dto.AuthResponse;
+import com.alwis.identity.auth.dto.LoginRequest;
+import com.alwis.identity.auth.dto.RegisterRequest;
+import com.alwis.identity.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
--- src/main/java/com/alwis/identity/auth/service/AuthServiceImpl.java
+++ src/main/java/com/alwis/identity/auth/service/AuthServiceImpl.java
@@ -1,9 +1,52 @@
 package com.alwis.identity.auth.service;
 
+import com.alwis.identity.auth.dto.AuthResponse;
+import com.alwis.identity.auth.dto.LoginRequest;
+import com.alwis.identity.auth.dto.RegisterRequest;
+import com.alwis.identity.auth.entity.Role;
+import com.alwis.identity.auth.entity.User;
+import com.alwis.identity.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthServiceImpl {
+public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public String register(RegisterRequest request) {
+
+        if(userRepository.existsByUsername(request.getUsername())){
+            throw new RuntimeException("Username already exists");
+        }
+
+        User user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        return "User registered successfully";
+    }
+
+    @Override
+    public AuthResponse login(LoginRequest request) {
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if(!passwordEncoder.matches(request.getPassword(),user.getPassword())){
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        // JWT will be added in next step
+        return new AuthResponse("Login successful (token coming next)");
+    }
 }
